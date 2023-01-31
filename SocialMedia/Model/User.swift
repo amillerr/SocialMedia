82b9b8fb7439732cce7cdde9e91d7314fe95869c
Tom Miller <62253468+amillerr@users.noreply.github.com>
2023-01-31 17:57:51 +0300

    feat: user model

---
+++ SocialMedia/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  SocialMedia
+//
+//  Created by Artem Axenov on 2023-01-31.
+//
+
+import SwiftUI
+import FirebaseFirestoreSwift
+
+struct User: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
