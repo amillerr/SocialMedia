572e4f0d3beb8c481d9ffb176021c6509cb0e84f
Tom Miller <62253468+amillerr@users.noreply.github.com>
2023-02-13 13:50:55 +0300

    feat: add post model

---
+++ SocialMedia/Model/Post.swift
@@ -0,0 +1,39 @@
+//
+//  Post.swift
+//  SocialMedia
+//
+//  Created by Artem Axenov on 2023-02-13.
+//
+
+import SwiftUI
+import FirebaseFirestoreSwift
+
+//MARK: Post model
+
+struct Post: Identifiable, Codable {
+    @DocumentID var id: String?
+    var text: String
+    var imageURL: URL?
+    var imageReferenceID: String = ""
+    var publishedDate: Date = Date()
+    var likedIDs: [String] = []
+    var dislikedIDs: [String] = []
+    
+    //MARK: Basic user info
+    var userName: String
+    var userUID: String
+    var userProfileURL: URL
+    
+    enum CodingKeys: CodingKey {
+        case id
+        case text
+        case imageURL
+        case imageReferenceID
+        case publishedDate
+        case likedIDs
+        case dislikedIDs
+        case userName
+        case userUID
+        case userProfileURL
+    }
+}
