a525e7396c5165fa6951d9c6fc2d134cafbcef42
Tom Miller <62253468+amillerr@users.noreply.github.com>
2023-02-18 12:23:32 +0300

    feat: displaying posts and scroll refresh

--- SocialMedia/View/MainView/PostsView.swift
+++ SocialMedia/View/MainView/PostsView.swift
@@ -8,29 +8,32 @@
 import SwiftUI
 
 struct PostsView: View {
+    @State private var recentPosts: [Post] = []
     @State private var createNewPost: Bool = false
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .hAlign(.center).vAlign(.center)
-            .overlay(alignment: .bottomTrailing) {
-                Button {
-                    createNewPost.toggle()
-                } label: {
-                    Image(systemName: "plus")
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .padding(13)
-                        .background(.black, in: Circle())
+        NavigationStack {
+            ReusablePostView(posts: $recentPosts)
+                .hAlign(.center).vAlign(.center)
+                .overlay(alignment: .bottomTrailing) {
+                    Button {
+                        createNewPost.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding(13)
+                            .background(.black, in: Circle())
+                    }
+                    .padding(15)
                 }
-                .padding(15)
+                .navigationTitle("Recent posts")
+        }
+        .fullScreenCover(isPresented: $createNewPost) {
+            CreateNewPost { post in
             }
-            .fullScreenCover(isPresented: $createNewPost) {
-                CreateNewPost { post in
-                    
-                }
-            }
+        }
     }
 }
 
---
+++ SocialMedia/View/MainView/ReusablePostView.swift
@@ -0,0 +1,83 @@
+//
+//  ReusablePostView.swift
+//  SocialMedia
+//
+//  Created by Artem Axenov on 2023-02-13.
+//
+
+import SwiftUI
+import Firebase
+import FirebaseFirestore
+
+struct ReusablePostView: View {
+    @Binding var posts: [Post]
+    /// - View properties
+    @State var isFetching: Bool = true
+
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            LazyVStack {
+                if isFetching {
+                    ProgressView()
+                        .padding(.top, 30)
+                } else {
+                    if posts.isEmpty {
+                        /// No Posts found on Firestore
+                        Text("No posts found")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                            .padding(.top, 30)
+                    } else {
+                        /// Displaying Posts
+                        Posts()
+                    }
+                }
+            }
+        }
+        .refreshable {
+            /// Scroll to refresh
+            isFetching = true
+            posts = []
+            await fetchPosts()
+        }
+        .task {
+            /// Fetching for one time
+            guard posts.isEmpty else { return }
+            await fetchPosts()
+        }
+    }
+    
+    /// - Displaying fetched posts
+    @ViewBuilder
+    func Posts() -> some View {
+        ForEach(posts) { post in
+            Text(post.text)
+        }
+    }
+    
+    /// - Fetching posts
+    func fetchPosts() async {
+        do {
+            var query: Query!
+            query = Firestore.firestore().collection("Posts")
+                .order(by: "publishedDate", descending: true)
+                .limit(to: 20)
+            let docs = try await query.getDocuments()
+            let fetchedPosts = docs.documents.compactMap { doc -> Post? in
+                try? doc.data(as: Post.self)
+            }
+            await MainActor.run(body: {
+                posts = fetchedPosts
+                isFetching = false
+            })
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+struct ReusablePostView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
